f9d094c87e8ca1c9ce5ad181c998c7d74614021c
kinan11 <80329022+kinan11@users.noreply.github.com>
2021-05-15 22:31:45 +0200

    Update GUI.h

--- GUI.h
+++ GUI.h
@@ -5,31 +5,24 @@
 // PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
-#ifndef __GUI_H__
-#define __GUI_H__
+#pragma once
 
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
-#include <wx/string.h>
-#include <wx/button.h>
+#include <wx/sizer.h>
 #include <wx/gdicmn.h>
+#include <wx/panel.h>
 #include <wx/font.h>
 #include <wx/colour.h>
 #include <wx/settings.h>
-#include <wx/checkbox.h>
-#include <wx/sizer.h>
-#include <wx/stattext.h>
-#include <wx/panel.h>
-#include <wx/frame.h>
-#include <wx/gauge.h>
+#include <wx/string.h>
+#include <wx/scrolbar.h>
+#include <wx/grid.h>
 #include <wx/bitmap.h>
 #include <wx/image.h>
 #include <wx/icon.h>
-#include <wx/textctrl.h>
-#include <wx/grid.h>
-#include <map>
-#include <wx/scrolbar.h>
-
+#include <wx/button.h>
+#include <wx/frame.h>
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -39,27 +32,29 @@
 ///////////////////////////////////////////////////////////////////////////////
 class MyFrame1 : public wxFrame
 {
-public:
+	private:
 
-	MyFrame1(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(643, 478), long style = wxDEFAULT_FRAME_STYLE | wxTAB_TRAVERSAL);
+	protected:
+		wxPanel* m_panel1;
+		wxScrollBar* m_scrollBar1;
+		wxGrid* IPTC;
+		wxGrid* EXIF;
+		wxButton* reset;
+		wxButton* A;
+		wxButton* B;
+		wxButton* D;
 
-	~MyFrame1();
+		// Virtual event handlers, overide them in your derived class
+		virtual void window_update( wxUpdateUIEvent& event ) { event.Skip(); }
+		virtual void scrollbar_scroll( wxScrollEvent& event ) { event.Skip(); }
+		virtual void LoadImgOnClick( wxCommandEvent& event ) { event.Skip(); }
 
 
-protected:
-	wxPanel* m_panel1;
-	wxScrollBar* m_scrollBar1;
-	wxGrid* IPTC;
-	wxGrid* EXIF;
-	wxButton* reset;
-	wxButton* A;
-	wxButton* B;
-	wxButton* D;
+	public:
 
-	virtual void window_update(wxUpdateUIEvent& event) { event.Skip(); }
-	virtual void scrollbar_scroll( wxScrollEvent& event ) { event.Skip(); }
-	virtual void LoadImgOnClick(wxCommandEvent& event) { event.Skip(); }
+		MyFrame1( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 643,478 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
+
+		~MyFrame1();
 
 };
 
-#endif 
