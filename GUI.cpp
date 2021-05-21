a510ccf9e68b0c528ad09633a6f0be0d5187841f
kinan11 <80329022+kinan11@users.noreply.github.com>
2021-05-21 15:22:38 +0200

    Update GUI.cpp

--- GUI.cpp
+++ GUI.cpp
@@ -7,192 +7,174 @@
 
 #include "GUI.h"
 
+
 ///////////////////////////////////////////////////////////////////////////
 
-MyFrame1::MyFrame1( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxFrame( parent, id, title, pos, size, style )
+MyFrame1::MyFrame1(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style) : wxFrame(parent, id, title, pos, size, style)
 {
-	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	this->SetSizeHints(wxDefaultSize, wxDefaultSize);
 
 	wxBoxSizer* bSizer1;
-	bSizer1 = new wxBoxSizer( wxHORIZONTAL );
+	bSizer1 = new wxBoxSizer(wxHORIZONTAL);
 
 	wxBoxSizer* bSizer2;
-	bSizer2 = new wxBoxSizer( wxHORIZONTAL );
+	bSizer2 = new wxBoxSizer(wxHORIZONTAL);
 
-	m_panel1 = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
-	m_panel1->SetBackgroundColour( wxColour( 255, 255, 255 ) );
-
-	wxFlexGridSizer* fgSizer1;
-	fgSizer1 = new wxFlexGridSizer( 0, 2, 0, 0 );
-	fgSizer1->SetFlexibleDirection( wxBOTH );
-	fgSizer1->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
-
-
-	m_panel1->SetSizer( fgSizer1 );
+	m_panel1 = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+	m_panel1->SetBackgroundColour(wxColour(255, 255, 255));
 	m_panel1->Layout();
-	fgSizer1->Fit( m_panel1 );
-	bSizer2->Add( m_panel1, 1, wxALL|wxEXPAND, 5 );
-
-	m_scrollBar1 = new wxScrollBar( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSB_VERTICAL );
-	bSizer2->Add( m_scrollBar1, 0, wxALL|wxEXPAND, 5 );
+	bSizer2->Add(m_panel1, 1, wxALL | wxEXPAND, 5);
 
 
-	bSizer1->Add( bSizer2, 1, wxEXPAND, 5 );
+	//panel full dsplay
+	wxBoxSizer* bSizer3;
+	bSizer3 = new wxBoxSizer(wxHORIZONTAL);
+	m_panelFullDisplay = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+	m_panelFullDisplay->SetBackgroundColour(wxColour(255, 255, 255));
+	m_panelFullDisplay->Layout();
+	bSizer3->Add(m_panelFullDisplay, 1, wxALL | wxEXPAND, 5);
+	m_panelFullDisplay->Hide();
+
+
+
+	bSizer1->Add(bSizer2, 1, wxEXPAND, 5);
 
 	wxBoxSizer* bSizer4;
-	bSizer4 = new wxBoxSizer( wxVERTICAL );
+	bSizer4 = new wxBoxSizer(wxVERTICAL);
 
 	wxBoxSizer* bSizer7;
-	bSizer7 = new wxBoxSizer( wxVERTICAL );
+	bSizer7 = new wxBoxSizer(wxVERTICAL);
 
 	wxBoxSizer* bSizer11;
-	bSizer11 = new wxBoxSizer( wxHORIZONTAL );
+	bSizer11 = new wxBoxSizer(wxHORIZONTAL);
 
-	IPTC = new wxGrid( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
+	IPTC = new wxGrid(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0);
 
 	// Grid
-	IPTC->CreateGrid( 5, 1 );
-	IPTC->EnableEditing( true );
-	IPTC->EnableGridLines( true );
-	IPTC->EnableDragGridSize( false );
-	IPTC->SetMargins( 0, 0 );
+	IPTC->CreateGrid(5, 1);
+	IPTC->EnableEditing(true);
+	IPTC->EnableGridLines(true);
+	IPTC->EnableDragGridSize(false);
+	IPTC->SetMargins(0, 0);
 
 	// Columns
-	IPTC->SetColSize( 0, 242 );
-	IPTC->EnableDragColMove( false );
-	IPTC->EnableDragColSize( false );
-	IPTC->SetColLabelSize( 0 );
-	IPTC->SetColLabelValue( 0, wxT("Values") );
-	IPTC->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+	IPTC->SetColSize(0, 242);
+	IPTC->EnableDragColMove(false);
+	IPTC->EnableDragColSize(false);
+	IPTC->SetColLabelSize(0);
+	IPTC->SetColLabelValue(0, wxT("Values"));
+	IPTC->SetColLabelAlignment(wxALIGN_CENTER, wxALIGN_CENTER);
 
 	// Rows
 	IPTC->AutoSizeRows();
-	IPTC->EnableDragRowSize( true );
-	IPTC->SetRowLabelSize( 130 );
-	IPTC->SetRowLabelValue( 0, wxT("Title") );
-	IPTC->SetRowLabelValue( 1, wxT("Date created") );
-	IPTC->SetRowLabelValue( 2, wxT("City") );
-	IPTC->SetRowLabelValue( 3, wxT("Country") );
-	IPTC->SetRowLabelValue( 4, wxT("Creator") );
-	IPTC->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+	IPTC->EnableDragRowSize(true);
+	IPTC->SetRowLabelSize(130);
+	IPTC->SetRowLabelValue(0, wxT("Title"));
+	IPTC->SetRowLabelValue(1, wxT("Date created"));
+	IPTC->SetRowLabelValue(2, wxT("City"));
+	IPTC->SetRowLabelValue(3, wxT("Country"));
+	IPTC->SetRowLabelValue(4, wxT("Creator"));
+	IPTC->SetRowLabelAlignment(wxALIGN_CENTER, wxALIGN_CENTER);
 
 	// Label Appearance
 
 	// Cell Defaults
-	IPTC->SetDefaultCellAlignment( wxALIGN_CENTER, wxALIGN_TOP );
-	bSizer11->Add( IPTC, 0, wxALIGN_TOP|wxALL, 5 );
+	IPTC->SetDefaultCellAlignment(wxALIGN_CENTER, wxALIGN_TOP);
+	bSizer11->Add(IPTC, 0, wxALIGN_TOP | wxALL, 5);
 
 
-	bSizer7->Add( bSizer11, 0, wxEXPAND, 5 );
+	bSizer7->Add(bSizer11, 0, wxEXPAND, 5);
 
 	wxBoxSizer* bSizer10;
-	bSizer10 = new wxBoxSizer( wxHORIZONTAL );
+	bSizer10 = new wxBoxSizer(wxHORIZONTAL);
 
-	EXIF = new wxGrid( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
+	EXIF = new wxGrid(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0);
 
 	// Grid
-	EXIF->CreateGrid( 16, 1 );
-	EXIF->EnableEditing( false );
-	EXIF->EnableGridLines( true );
-	EXIF->EnableDragGridSize( false );
-	EXIF->SetMargins( 0, 0 );
+	EXIF->CreateGrid(16, 1);
+	EXIF->EnableEditing(false);
+	EXIF->EnableGridLines(true);
+	EXIF->EnableDragGridSize(false);
+	EXIF->SetMargins(0, 0);
 
 	// Columns
-	EXIF->SetColSize( 0, 242 );
-	EXIF->EnableDragColMove( false );
-	EXIF->EnableDragColSize( false );
-	EXIF->SetColLabelSize( 0 );
-	EXIF->SetColLabelValue( 0, wxT("Values") );
-	EXIF->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+	EXIF->SetColSize(0, 242);
+	EXIF->EnableDragColMove(false);
+	EXIF->EnableDragColSize(false);
+	EXIF->SetColLabelSize(0);
+	EXIF->SetColLabelValue(0, wxT("Values"));
+	EXIF->SetColLabelAlignment(wxALIGN_CENTER, wxALIGN_CENTER);
 
 	// Rows
 	EXIF->AutoSizeRows();
-	EXIF->EnableDragRowSize( true );
-	EXIF->SetRowLabelSize( 130 );
-	EXIF->SetRowLabelValue( 0, wxT("Camera make") );
-	EXIF->SetRowLabelValue( 1, wxT("Camera model") );
-	EXIF->SetRowLabelValue( 2, wxT("Date/Time") );
-	EXIF->SetRowLabelValue( 3, wxT("Image description") );
-	EXIF->SetRowLabelValue( 4, wxT("Orientation") );
-	EXIF->SetRowLabelValue( 5, wxT("X resolution [dpi]") );
-	EXIF->SetRowLabelValue( 6, wxT("Y resolution [dpi]") );
-	EXIF->SetRowLabelValue( 7, wxT("Resolution unit") );
-	EXIF->SetRowLabelValue( 8, wxT("Software") );
-	EXIF->SetRowLabelValue( 9, wxT("White point") );
-	EXIF->SetRowLabelValue( 10, wxT("Primary Chomaticities") );
-	EXIF->SetRowLabelValue( 11, wxT("YCbCr Coefficients") );
-	EXIF->SetRowLabelValue( 12, wxT("YCbCr Positioning") );
-	EXIF->SetRowLabelValue( 13, wxT("Reference BlackWhite") );
-	EXIF->SetRowLabelValue( 14, wxT("Copyright") );
-	EXIF->SetRowLabelValue( 15, wxT("Exif Offset") );
-	EXIF->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+	EXIF->EnableDragRowSize(true);
+	EXIF->SetRowLabelSize(130);
+	EXIF->SetRowLabelValue(0, wxT("Camera make"));
+	EXIF->SetRowLabelValue(1, wxT("Camera model"));
+	EXIF->SetRowLabelValue(2, wxT("Date/Time"));
+	EXIF->SetRowLabelValue(3, wxT("Image description"));
+	EXIF->SetRowLabelValue(4, wxT("Orientation"));
+	EXIF->SetRowLabelValue(5, wxT("X resolution [dpi]"));
+	EXIF->SetRowLabelValue(6, wxT("Y resolution [dpi]"));
+	EXIF->SetRowLabelValue(7, wxT("Resolution unit"));
+	EXIF->SetRowLabelValue(8, wxT("Software"));
+	EXIF->SetRowLabelValue(9, wxT("White point"));
+	EXIF->SetRowLabelValue(10, wxT("Primary Chomaticities"));
+	EXIF->SetRowLabelValue(11, wxT("YCbCr Coefficients"));
+	EXIF->SetRowLabelValue(12, wxT("YCbCr Positioning"));
+	EXIF->SetRowLabelValue(13, wxT("Reference BlackWhite"));
+	EXIF->SetRowLabelValue(14, wxT("Copyright"));
+	EXIF->SetRowLabelValue(15, wxT("Exif Offset"));
+	EXIF->SetRowLabelAlignment(wxALIGN_CENTER, wxALIGN_CENTER);
 
 	// Label Appearance
 
 	// Cell Defaults
-	EXIF->SetDefaultCellAlignment( wxALIGN_CENTER, wxALIGN_TOP );
-	bSizer10->Add( EXIF, 0, wxALL, 5 );
+	EXIF->SetDefaultCellAlignment(wxALIGN_CENTER, wxALIGN_TOP);
+	bSizer10->Add(EXIF, 0, wxALL, 5);
 
 
-	bSizer7->Add( bSizer10, 1, wxEXPAND, 5 );
+	bSizer7->Add(bSizer10, 1, wxEXPAND, 5);
 
 
-	bSizer4->Add( bSizer7, 1, wxEXPAND, 5 );
+	bSizer4->Add(bSizer7, 1, wxEXPAND, 5);
 
 	wxBoxSizer* bSizer9;
-	bSizer9 = new wxBoxSizer( wxVERTICAL );
+	bSizer9 = new wxBoxSizer(wxVERTICAL);
 
-	reset = new wxButton( this, wxID_ANY, wxT("Resetuj Exif"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer9->Add( reset, 0, wxALIGN_CENTER|wxALL, 5 );
+	reset = new wxButton(this, wxID_ANY, wxT("Resetuj Exif"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer9->Add(reset, 0, wxALIGN_CENTER | wxALL, 5);
 
-	A = new wxButton( this, wxID_ANY, wxT("Wygeneruj plik"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer9->Add( A, 0, wxALIGN_CENTER|wxALL, 5 );
+	A = new wxButton(this, wxID_ANY, wxT("Wygeneruj plik"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer9->Add(A, 0, wxALIGN_CENTER | wxALL, 5);
 
-	B = new wxButton( this, wxID_ANY, wxT("Zapisz tekst"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer9->Add( B, 0, wxALIGN_CENTER|wxALL, 5 );
+	B = new wxButton(this, wxID_ANY, wxT("Zapisz tekst"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer9->Add(B, 0, wxALIGN_CENTER | wxALL, 5);
 
-	D = new wxButton( this, wxID_ANY, wxT("Wczytaj plik"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer9->Add( D, 0, wxALIGN_CENTER|wxALL, 5 );
-
-
-	bSizer4->Add( bSizer9, 0, wxALIGN_CENTER, 5 );
+	D = new wxButton(this, wxID_ANY, wxT("Wczytaj folder"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer9->Add(D, 0, wxALIGN_CENTER | wxALL, 5);
 
 
-	bSizer1->Add( bSizer4, 0, wxEXPAND, 5 );
+	bSizer4->Add(bSizer9, 0, wxALIGN_CENTER, 5);
 
 
-	this->SetSizer( bSizer1 );
+	bSizer1->Add(bSizer4, 0, wxEXPAND, 5);
+
+
+	this->SetSizer(bSizer1);
 	this->Layout();
 
-	this->Centre( wxBOTH );
+	this->Centre(wxBOTH);
 
 	// Connect Events
-	this->Connect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( MyFrame1::window_update ) );
-	m_scrollBar1->Connect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Connect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Connect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Connect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Connect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Connect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Connect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Connect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Connect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	D->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame1::LoadImgOnClick ), NULL, this );
+	this->Connect(wxEVT_UPDATE_UI, wxUpdateUIEventHandler(MyFrame1::window_update));
+	D->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::LoadImgOnClick), NULL, this);
 }
 
 MyFrame1::~MyFrame1()
 {
 	// Disconnect Events
-	this->Disconnect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( MyFrame1::window_update ) );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_TOP, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_BOTTOM, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_LINEUP, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_LINEDOWN, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_PAGEUP, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_PAGEDOWN, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_THUMBTRACK, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_THUMBRELEASE, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	m_scrollBar1->Disconnect( wxEVT_SCROLL_CHANGED, wxScrollEventHandler( MyFrame1::scrollbar_scroll ), NULL, this );
-	D->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame1::LoadImgOnClick ), NULL, this );
+	this->Disconnect(wxEVT_UPDATE_UI, wxUpdateUIEventHandler(MyFrame1::window_update));
+	D->Disconnect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::LoadImgOnClick), NULL, this);
 
 }
