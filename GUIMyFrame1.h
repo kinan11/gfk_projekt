5689010effbfd7ac90110812acf1602cc22155c0
kinan11 <80329022+kinan11@users.noreply.github.com>
2021-05-22 15:15:01 +0200

    Update GUIMyFrame1.h

--- GUIMyFrame1.h
+++ GUIMyFrame1.h
@@ -38,34 +38,6 @@ public:
 	int _yDownRigth;
 };
 
-/*
-class Thumbnail{
-public:
-	Thumbnail(wxWindow* parent, wxString fileName)
-	{
-		m_filename = fileName;
-		wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
-
-		wxImage image=wxImage(m_filename);
-		m_bitmap = wxBitmap(image,-1);
-		wxBitmap smallBmp = m_bitmap;
-		//m_bitmapButton = new wxBitmapButton(this, wxID_ANY, smallBmp);
-		//sizer.Add(m_bitmapButton, 1, wxCenter,3);
-		//wxEVT_BUTTON(-1, new wxEventListener(OnBitmapButtonClicked));
-	}
-protected:
-	//void OnBitmapButtonClicked(wxObject& event, wxEvent& event){
-		// Notify our listeners
-		/*if (ThumbnailClicked != null) {
-			ThumbnailClicked(m_fileName, m_bitmap);
-		}
-	//}
-private:
-	wxBitmap m_bitmap;
-	wxString m_filename;
-	wxBitmapButton *m_bitmapButton;
-};*/
-
 
 /** Implementing MyFrame1 */
 class GUIMyFrame1 : public MyFrame1
@@ -107,7 +79,7 @@ public:
 	//static methods for displaying data
 	static void DisplayPic(wxPanel* parent, wxString path, wxPanel* display, wxFlexGridSizer* fgSizer);
 	static void DisplayMetaData(wxGrid* EXIF, wxPanel* parent, wxPanel* display, wxString path);
-	static void DisplayFolder(wxPanel* parent, wxPanel* display);
+	static void DisplayFolder(wxPanel* parent, wxPanel* display, wxString &path);
 	//FreeImage
 	void OnPanelClick(wxMouseEvent& event)
 	{
@@ -127,14 +99,12 @@ class Panel2 : public wxPanel{
 	friend class GUIMyFrame1;
 	friend class MyCanvas;
 	public:
-		Panel2(wxPanel* parent, wxString path, wxPanel* display, wxFlexGridSizer* fgSizer, const wxSize& pos = wxDefaultSize):wxPanel(display, -1, wxPoint(0, 0),pos),_parent(parent),_path(path),_DisplayPanel(display),_fgSizer(fgSizer)
-		{	
+		Panel2(wxPanel* parent, wxString path, wxPanel* display, wxFlexGridSizer* fgSizer, const wxSize& pos = wxDefaultSize) :wxPanel(display, -1, wxPoint(0, 0), pos), _parent(parent), _path(path), _DisplayPanel(display), _fgSizer(fgSizer)
+		{
 			Connect(wxEVT_LEFT_DCLICK, wxMouseEventHandler(Panel2::OnPanelDoubleDown));
-			wxClientDC dc(display);
-			wxImage im = wxImage(path, wxBITMAP_TYPE_ANY, -1);
-			im.Rescale(1200, 700, wxIMAGE_QUALITY_NEAREST);
-			wxBitmap b1(im, -1);
-			dc.DrawBitmap(b1, 0, 0);
+			Connect(wxEVT_PAINT, (wxObjectEventFunction)&Panel2::OnPaint);
+			display->Show();
+			display->SetBackgroundColour(wxBG_STYLE_CUSTOM);			
 		}
 	private:
 		wxPanel* _parent;
@@ -143,8 +113,16 @@ class Panel2 : public wxPanel{
 		wxFlexGridSizer* _fgSizer;
 		void OnPanelDoubleDown(wxMouseEvent& event)
 		{
-			GUIMyFrame1::DisplayFolder(_parent,_DisplayPanel);
+			GUIMyFrame1::DisplayFolder(_parent,_DisplayPanel,_path);
 			
+		}
+		void OnPaint(wxPaintEvent& event)
+		{
+			wxImage im = wxImage(_path, wxBITMAP_TYPE_ANY, -1);
+			im.Rescale(1200, 700, wxIMAGE_QUALITY_NEAREST);
+			wxBitmap b1(im, -1);
+			wxPaintDC dc(this);
+			dc.DrawBitmap(b1, 0, 0, false);
 		}
 	
 };
@@ -181,60 +159,6 @@ private:
 	void OnMouseDoubleDown(wxMouseEvent& event)
 	{
 		GUIMyFrame1::DisplayPic(parent1, path, DisplayPanel, fgSizer);
-	}
-};
-
-class Scrolling : public wxScrolledWindow
-{
-	wxBitmap* bitmap;
-	int w, h;
-	Scrolling* m_peer;
-
-public:
-	Scrolling(wxWindow* parent, wxWindowID id, int width,int height) : wxScrolledWindow(parent, id)
-	{
-		m_peer = NULL;
-
-		w = width;			//DO NOT DELETE!
-		h = height;
-
-		/* init scrolled area size, scrolling speed, etc. */
-		SetScrollbars(1, 1, w, h, 0, 0);
-
-		//bitmap = new wxBitmap(image);
-
-		Connect(wxID_ANY, wxEVT_SCROLLWIN_THUMBTRACK, wxScrollWinEventHandler(Scrolling::onScroll), NULL, this);
-		Connect(wxID_ANY, wxEVT_SCROLLWIN_THUMBRELEASE, wxScrollWinEventHandler(Scrolling::onScroll), NULL, this);
-		Connect(wxID_ANY, wxEVT_SCROLLWIN_LINEUP, wxScrollWinEventHandler(Scrolling::onScroll), NULL, this);
-		Connect(wxID_ANY, wxEVT_SCROLLWIN_LINEDOWN, wxScrollWinEventHandler(Scrolling::onScroll), NULL, this);
-	}
-	~Scrolling()
-	{
-		//delete bitmap;
-	}
-
-	void SetPeer(Scrolling* other)
-	{
-		m_peer = other;
-	}
-
-	void onScroll(wxScrollWinEvent& evt)
-	{
-		if (m_peer != NULL)
-		{
-			m_peer->Scroll(GetScrollPos(wxHORIZONTAL), GetScrollPos(wxVERTICAL));
-		}
-		evt.Skip(); // let the event go
-	}
-
-	void OnDraw(wxDC& dc)
-	{
-		/* render the image - in a real app, if your scrolled area
-		 is somewhat big, you will want to draw only visible parts,
-		 not everything like below */
-		//dc.DrawBitmap(*bitmap, 0, 0, false);
-
-		// also check wxScrolledWindow::PrepareDC
 	}
 };
 
